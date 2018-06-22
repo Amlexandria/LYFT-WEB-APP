6bd0f272b6200303ee72a9fe411409506b607472
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-06-22 00:28:19 -0500

    Se termina la funcinalidad de los inputs de la vista 3: Sing Up

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
-// $('.dropdown-trigger').dropdown();
-
+// MODAL
 $(document).ready(function(){
     $('.modal').modal();
   });
 
+$(document).ready(function() {
+    $('input#phone-number').characterCounter();
+  });
+
+// -------------VALIDACIONES SING UP------------------
+
+function digitCounter () {
+	var userPhoneNumber = $('#phone-number').val();
+	if(userPhoneNumber.length === 10){
+		$('#phone-number').addClass('validate')
+		$('#btn-next').removeClass('disabled');
+	}
+};
+// function getPhoneNumber () {
+// 	var userPhoneNumber = $('#phone').val();
+
+// 	if(userPhoneNumber.length === 10){
+// 		$('#btn-next').removeClass('disabled');
+// 	}else {
+// 		swal("Oh no!", "Please enter the 10 digits of your phone number!", "error");
+// 	};
+
+// };
+
+
+
+
+
+
+
+
+
  $(document).ready(function(){
     $('select').formSelect();
+    $('#phone-number').mousemove(digitCounter);
+
   });
