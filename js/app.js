a99e644566ccbf24b9a0cba10b35c22921a1f77f
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-06-22 02:13:00 -0500

    Se genera el código aleatorio y se muestra en un alert

--- js/app.js
+++ js/app.js
@@ -10,25 +10,32 @@ $(document).ready(function() {
 // -------------VALIDACIONES SING UP------------------
 
 function digitCounter () {
+	// creo una variable para obtener el value del input
 	var userPhoneNumber = $('#phone-number').val();
+	// condiciono a que cuando el length del value sea igual a 10...
 	if(userPhoneNumber.length === 10){
-		$('#phone-number').addClass('validate')
+		// valide el el value
+		$('#phone-number').addClass('validate');
+		// se active el botón "Next"
 		$('#btn-next').removeClass('disabled');
 	}
 };
-// function getPhoneNumber () {
-// 	var userPhoneNumber = $('#phone').val();
 
-// 	if(userPhoneNumber.length === 10){
-// 		$('#btn-next').removeClass('disabled');
-// 	}else {
-// 		swal("Oh no!", "Please enter the 10 digits of your phone number!", "error");
-// 	};
+// ------------GENERACIÓN DE CÓDIGO ALEATORIO-----------
+// CODIGO ASCII
+// Números: 48-57
 
-// };
+function randomCode () {
 
+	var code = "";
 
-
+	for(var i=0; i<=2; i++){
+		var character = String.fromCharCode(Math.floor(Math.random() * (48 - 57) + 57));
+		code += character;
+	}
+console.log(code);
+swal("HWe have created a code for you!", "Your code: LAB-" + code);
+}
 
 
 
@@ -38,5 +45,6 @@ function digitCounter () {
  $(document).ready(function(){
     $('select').formSelect();
     $('#phone-number').mousemove(digitCounter);
+    $('#btn-next').click(randomCode);
 
   });
